07e7376327a721f7e258faaa0c3007d2f0ed0884
leecr1215 <leecr1215@naver.com>
2022-02-12 17:43:58 +0900

    feat: startpage finish with styles component

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { Image } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Image style={styles.logo} source={require("./icon/logo_white.png")} />
+      </View>
+      <Text style={styles.appText}>CHERISH PET!</Text>
       <StatusBar style="auto" />
+      <View style={styles.startBtn}>
+        <TouchableOpacity>
+          <Text style={styles.startText}>시작하기</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +30,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#D8E1FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  appText: {
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  startBtn: {
+    backgroundColor: "white",
+    alignItems: "center",
+    paddingBottom: 7,
+    paddingTop: 7,
+    borderRadius: 50,
+    width: SCREEN_WIDTH * 0.3,
+    shadowColor: "#000000",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 2, height: 2 },
+    elevation: 3,
+    marginTop: SCREEN_WIDTH * 0.2,
+  },
+  startText: { fontSize: 16 },
+  logo: {
+    resizeMode: "contain",
+    width: SCREEN_WIDTH * 0.4,
+    height: SCREEN_WIDTH * 0.5,
   },
 });
