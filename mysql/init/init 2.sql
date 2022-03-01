0f5438a7373aee0159aeb53eed71002dd0536b44
김송이 <ksl2950@o.cnu.ac.kr>
2022-03-01 15:25:31 +0900

    fix : bug fix

--- mysql/init/init 2.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE cherishdata;
-CREATE user cherishpet@localhost identified by 'thddlcofla';
-GRANT ALL PRIVILEGES ON cherishdata.* TO 'cherishpet'@'%';
-SET PASSWORD FOR 'cherishpet'@'localhost' = 'thddlcofla';
-
-USE cherishdata;
